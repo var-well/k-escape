7ca9f725426788f5373b2d1a53521fd80aca75b5
LeeYu <reasonz@daum.net>
2020-04-17 19:01:32 +0900

    add

--- js/page1.js
+++ js/page1.js
@@ -1,3 +1,25 @@
+window.addEventListener("load", function(){
+    (function(window, document){
+        'use strict';
+    
+        const toggles = document.querySelectorAll('.toggle');
+        const toggleBtn = document.getElementById('toggle-btn');
+    
+        toggleBtn.addEventListener('click', function(){
+            toggleElements();
+        });
+    
+        function toggleElements(){
+            [].forEach.call(toggles, function(toggle){
+                toggle.classList.toggle('on');
+            });
+        };
+    
+    })(window, document)
+    
+});
+
+
 $(document).ready(function(){
 
     $('ul.tabs__list li').click(function(){
@@ -9,5 +31,3 @@ $(document).ready(function(){
     })
 
 })
-
-
